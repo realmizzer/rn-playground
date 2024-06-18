7d09cc920720665cfe36a3851594e35f12d36baf
realMiZZeR <diamondAlma32@gmail.com>
2024-06-18 18:04:05 +0700

    реализовал простенький пример с использованием react-native-reanimated

--- src/navigation/stacks/animation/screens/ReanimatedAnimationScreen.tsx
+++ src/navigation/stacks/animation/screens/ReanimatedAnimationScreen.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+} from 'react-native-reanimated';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 70,
+    fontFamily: 'Arial',
+  },
+});
 
 export const ReanimatedAnimationScreen = () => {
+  const opacity = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(
+    () => ({
+      opacity: opacity.value,
+    }),
+    [],
+  );
+
+  useEffect(() => {
+    opacity.value = withRepeat(withSpring(1, {duration: 5000}), -1);
+  }, [opacity]);
+
   return (
-    <SafeAreaView>
-      <Text>aboba</Text>
+    <SafeAreaView style={styles.container}>
+      <Animated.Text style={[styles.text, animatedStyles]}>ABOBA</Animated.Text>
     </SafeAreaView>
   );
 };
